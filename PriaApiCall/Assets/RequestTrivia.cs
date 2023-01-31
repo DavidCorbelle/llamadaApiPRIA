6c7eccc3e27694a64d011af4434f5bbc571fde4a
David Corbelle García <corgardav@IESPIRINGALLA.AD>
2023-01-31 19:20:29 +0100

    Agregadas las clases que se van a usar para utilizar las preguntas

---
+++ PriaApiCall/Assets/RequestTrivia.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RequestTrivia 
+{
+    public string response_code;
+    public List<Pregunta> results;
+}
